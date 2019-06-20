6e687017c76b2036a0876ab4c8e376d8223f0217
mpilo.dlamini@yahoo.com <mpilo.dlamini@yahoo.com>
2019-06-20 20:31:44 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotesProps="quotesArray"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+
+    export default {
+        data: function(){
+            return {
+                quotesArray: [
+                    'This is a quote for display purposes'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {  
+            newQuote(quoteArg) {
+                this.quotesArray.push(quoteArg)
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,21 @@
+<template>
+   <div class="row">
+       <app-quote v-for="quote in quotesProps">{{ quote }}</app-quote>
+   </div> 
+</template>
+
+<script>
+import Quote from './Quote.vue';
+export default {
+    props: ['quotesProps'],
+    components: {
+        appQuote: Quote
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
